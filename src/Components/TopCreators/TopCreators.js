1b0bd6c4e720f2afbdb06d3c542abe2b3e1f25ae
Muzammil Husnain <muzammilhusnain7@gmail.com>
2023-07-21 11:12:41 +0500

    Do Some changes

--- src/Components/TopCreators/TopCreators.js
+++ src/Components/TopCreators/TopCreators.js
@@ -1,4 +1,18 @@
 import TopCreatorsCards from "../TopCreatorsCards/TopCreatorsCards"
+// User Images
+import Img1 from '../../Images/Avatar .png'
+import Img2 from '../../Images/Avatar Placeholder-2.png'
+import Img3 from '../../Images/Avatar Placeholder-3.png'
+import Img4 from '../../Images/Avatar Placeholder-4.png'
+import Img5 from '../../Images/Avatar Placeholder-5.png'
+import Img6 from '../../Images/Avatar Placeholder-6.png'
+import Img7 from '../../Images/Avatar Placeholder-7.png'
+import Img8 from '../../Images/Avatar Placeholder-8.png'
+import Img9 from '../../Images/Avatar Placeholder-9.png'
+import Img10 from '../../Images/Avatar Placeholder-10.png'
+import Img11 from '../../Images/Avatar Placeholder-11.png'
+import Img12 from '../../Images/Avatar .png'
+
 
 export default function TopCreators() {
     return (
@@ -70,7 +84,7 @@ export default function TopCreators() {
                     
                 </div>
             </div>
-            
+
             <br />
 
             <div className="container">
--- src/Components/TopCreatorsCards/TopCreatorsCards.js
+++ src/Components/TopCreatorsCards/TopCreatorsCards.js
@@ -6,7 +6,7 @@ export default function TopCreatorsCards() {
            <center>
            <img src={Img} className="card-img-top" alt="..." style={{width: "4rem"}} />
                 <div className="card-body topCardBody">
-                    <h4 className=" text-white">Keepitreal</h4>
+                    <h5 className=" text-white">Keepitreal</h5>
                     <div className='d-flex'>
                     <p className="card-text text-white-50">Total Sales: </p>
                     <p className="card-text text-white"> 34.53 ETH</p>
