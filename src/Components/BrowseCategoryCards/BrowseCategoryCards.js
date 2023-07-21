36d4337d900fb3eb106b4c8f3bf04d038fed6b5d
Muzammil Husnain <muzammilhusnain7@gmail.com>
2023-07-21 22:32:25 +0500

    DO SOme Changes

--- src/Components/BrowseCategoryCards/BrowseCategoryCards.js
+++ src/Components/BrowseCategoryCards/BrowseCategoryCards.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function BrowseCategoryCards(props) {
     return (
-        <div class="card bg-secondary bg-gradient" style={{width: "10rem"}}>
+        <div class="card bg-secondary bg-gradient" style={{width: "12rem"}}>
             <img src={props.scr} class="card-img-top" alt="..."/>
                 <div class="card-body">
                     <p class="card-text text-white">{props.text}</p>
--- src/Components/HowItWorkCards/HowItWorkCards.js
+++ src/Components/HowItWorkCards/HowItWorkCards.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function HowItWorkCards(props) {
   return (
-    <div className="card bg-secondary bg-gradient" style={{width: "18rem"}}>
+    <div className="card bg-secondary bg-gradient" style={{width: "20rem"}}>
       <img src={props.scr} className="card-img-top" alt="..." />
       <div className="card-body">
         <center>
