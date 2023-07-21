d6522578397fc50c318f285bfc0fdd5b9707ecbf
Muzammil Husnain <muzammilhusnain7@gmail.com>
2023-07-22 00:39:03 +0500

    Do SOme Change

--- src/Components/MagicMashrooms/MagicMashrooms.js
+++ src/Components/MagicMashrooms/MagicMashrooms.js
@@ -1,4 +1,5 @@
 import Timer from "../Timer/Timer";
+import Img from '../../Images/Asset 12 2.png'
 
 
 export default function MagicMashrooms() {
@@ -6,8 +7,13 @@ export default function MagicMashrooms() {
     <div className="background-image-container">
       <div className="container">
   <div className="row">
-    <div class="col">
-      
+    <div class="col" style={{padding: "4rem"}}>
+        <div className="d-flex text-white" >
+            <img src={Img} alt="#" style={{ width: "1rem" , height: "1rem"}} /> 
+            <p>Shroomie</p>
+        </div>
+      <h4 className="text-white">Magic Mashrooms</h4>
+      <button type="button" class="btn btn-light"> <i class="bi bi-eye"> </i>See NFT</button>
     </div>
     <div className="col">
       <Timer />
