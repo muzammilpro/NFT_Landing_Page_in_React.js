67fa98c69c5bd488fe7ebe39bebe874ec326a98d
Muzammil Husnain <muzammilhusnain7@gmail.com>
2023-07-21 22:26:06 +0500

    Do Some Changes

--- src/Components/HowItWork/HowItWork.js
+++ src/Components/HowItWork/HowItWork.js
@@ -1,20 +1,24 @@
-import React from 'react'
+import HowItWorkCards from "../HowItWorkCards/HowItWorkCards"
+// Cards Images
+import Img1 from '../../Images/Icon.png'
+import Img2 from '../../Images/Icon (1).png'
+import Img3 from '../../Images/Icon (2).png'
 
 export default function HowItWork() {
     return (
         <div>
             <div className="container">
-                <h3 className='text-white'>How It Works</h3> <br/>
+                <h3 className='text-white'>How It Works</h3> <br />
                 <p className='text-white'>Find Out How TO Get Started</p>
                 <div className="row">
                     <div className="col">
-                        Column
+                        <HowItWorkCards  scr={Img1} title="Setup Your Wallet"  paragraph="Set up your wallet of choice.Connect it to the Animarket by clicking the wallet icon in the top right corner"/>
                     </div>
                     <div className="col">
-                        Column
+                        <HowItWorkCards scr={Img2} title="Create Collection" paragraph="Upload your work and setup your collection. Add a description, social links and floor price."/>
                     </div>
                     <div className="col">
-                        Column
+                        <HowItWorkCards scr={Img3} title="Start Earning" paragraph="Choose between auctions and fixed-price listing. Start earning by selling your NFTs or trading others "/>
                     </div>
                 </div>
             </div>
--- src/Components/HowItWorkCards/HowItWorkCards.js
+++ src/Components/HowItWorkCards/HowItWorkCards.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export default function HowItWorkCards() {
+export default function HowItWorkCards(props) {
   return (
-    <div>
-      
+    <div className="card bg-secondary bg-gradient" style={{width: "18rem"}}>
+      <img src={props.scr} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <center>
+        <h5 className='text-white'>{props.title}</h5>
+        <p className="card-text text-white">{props.paragraph}</p>
+        </center>
+      </div>
     </div>
   )
 }
